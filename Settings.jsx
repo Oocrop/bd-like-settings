391e5000f6cc0fce1a16d35e472f85b7182a2a53
Oocrop <0_0_0_0-0-0-0@mail.ru>
2020-10-10 13:41:02 +0700

    .

--- Settings.jsx
+++ Settings.jsx
@@ -1,4 +1,6 @@
-const { settings: { Category, SwitchItem } } = require("powercord/components");
+const {
+    settings: { Category, SwitchItem }
+} = require("powercord/components");
 const { React } = require("powercord/webpack");
 
 module.exports = class Settings extends React.Component {
@@ -11,23 +13,35 @@ module.exports = class Settings extends React.Component {
     render() {
         const { getSetting, toggleSetting } = this.props;
 
-        return <>
-            <SwitchItem
-                value={getSetting("hideSections", true)}
-                onChange={() => toggleSetting("hideSections", true)}
-            >Hide sections from settings menu</SwitchItem>
-            <Category
-                name="Which plugins should use section"
-                opened={this.state.pluginsCategoryOpen}
-                onChange={() => this.setState({ pluginsCategoryOpen: !this.state.pluginsCategoryOpen })}
-            >
-                {Object.keys(powercord.api.settings.tabs).filter(p => !p.startsWith("pc-")).map(p =>
-                    <SwitchItem
-                        value={getSetting(p, false)}
-                        onChange={() => toggleSetting(p, false)}
-                    >{powercord.api.settings.tabs[p].label}</SwitchItem>
-                )}
-            </Category>
-        </>;
+        return (
+            <>
+                <SwitchItem
+                    checked={getSetting("hideSections", true)}
+                    onChange={() => toggleSetting("hideSections", true)}
+                >
+                    Hide sections from settings menu
+                </SwitchItem>
+                <Category
+                    name="Which plugins should use section"
+                    opened={this.state.pluginsCategoryOpen}
+                    onChange={() =>
+                        this.setState({
+                            pluginsCategoryOpen: !this.state.pluginsCategoryOpen
+                        })
+                    }
+                >
+                    {Object.keys(powercord.api.settings.tabs)
+                        .filter(p => !p.startsWith("pc-"))
+                        .map(p => (
+                            <SwitchItem
+                                checked={getSetting(p, false)}
+                                onChange={() => toggleSetting(p, false)}
+                            >
+                                {powercord.api.settings.tabs[p].label}
+                            </SwitchItem>
+                        ))}
+                </Category>
+            </>
+        );
     }
-}
+};
