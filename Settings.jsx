aa09ec74b7819fdbeea941fc20f27c3b030aa467
Oocrop <0_0_0_0-0-0-0@mail.ru>
2020-12-21 18:36:34 +0700

    context menu fix

--- Settings.jsx
+++ Settings.jsx
@@ -1,7 +1,11 @@
-const { settings: { Category, SwitchItem } } = require("powercord/components");
+const {
+    settings: { Category, SwitchItem }
+} = require("powercord/components");
 const { React } = require("powercord/webpack");
 
-module.exports = class Settings extends React.Component {
+module.exports = class Settings extends (
+    React.Component
+) {
     constructor(...args) {
         super(...args);
 
@@ -11,23 +15,35 @@ module.exports = class Settings extends React.Component {
     render() {
         const { getSetting, toggleSetting } = this.props;
 
-        return <>
-            <SwitchItem
-                value={getSetting("hideSections", true)}
-                onChange={() => toggleSetting("hideSections", true)}
-            >Hide sections from settings menu</SwitchItem>
-            <Category
-                name="Which plugins should use section"
-                opened={this.state.pluginsCategoryOpen}
-                onChange={() => this.setState({ pluginsCategoryOpen: !this.state.pluginsCategoryOpen })}
-            >
-                {Object.keys(powercord.api.settings.tabs).filter(p => !p.startsWith("pc-")).map(p =>
-                    <SwitchItem
-                        value={getSetting(p, false)}
-                        onChange={() => toggleSetting(p, false)}
-                    >{powercord.api.settings.tabs[p].label}</SwitchItem>
-                )}
-            </Category>
-        </>;
+        return (
+            <>
+                <SwitchItem
+                    value={getSetting("hideSections", true)}
+                    onChange={() => toggleSetting("hideSections", true)}
+                >
+                    Hide sections from settings menu
+                </SwitchItem>
+                <Category
+                    name="Which plugins should use section"
+                    opened={this.state.pluginsCategoryOpen}
+                    onChange={() =>
+                        this.setState({
+                            pluginsCategoryOpen: !this.state.pluginsCategoryOpen
+                        })
+                    }
+                >
+                    {Object.keys(powercord.api.settings.tabs)
+                        .filter(p => !p.startsWith("pc-"))
+                        .map(p => (
+                            <SwitchItem
+                                value={getSetting(p, false)}
+                                onChange={() => toggleSetting(p, false)}
+                            >
+                                {powercord.api.settings.tabs[p].label}
+                            </SwitchItem>
+                        ))}
+                </Category>
+            </>
+        );
     }
-}
+};
--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 const { Plugin } = require("powercord/entities");
 const { inject, uninject } = require("powercord/injector");
-const { React, getModule, i18n: { Messages } } = require("powercord/webpack");
+const {
+    React,
+    getModule,
+    i18n: { Messages }
+} = require("powercord/webpack");
 const { findInReactTree } = require("powercord/util");
-const { Button, Card, Clickable, Divider, Icons: { Close }, Tooltip } = require("powercord/components");
+const {
+    Button,
+    Card,
+    Clickable,
+    Divider,
+    Icons: { Close },
+    Tooltip
+} = require("powercord/components");
 const PluginSettings = require("./Settings");
 const { resolve, join } = require("path");
 var _this;
 
-module.exports = class BDLikeSettings extends Plugin {
+module.exports = class BDLikeSettings extends (
+    Plugin
+) {
     async startPlugin() {
         _this = this;
 
@@ -20,16 +33,52 @@ module.exports = class BDLikeSettings extends Plugin {
         const moduleManagerPath = resolve(__dirname, "..", "pc-moduleManager");
         const settingsPath = resolve(__dirname, "..", "pc-settings");
 
-        const InstalledProduct = require(join(moduleManagerPath, "components/parts/InstalledProduct.jsx"));
+        const InstalledProduct = require(join(
+            moduleManagerPath,
+            "components/parts/InstalledProduct.jsx"
+        ));
         const Settings = require(join(settingsPath, "index.js"));
-        this.ErrorBoundary = require(join(settingsPath, "components/ErrorBoundary.jsx"));
+        this.ErrorBoundary = require(join(
+            settingsPath,
+            "components/ErrorBoundary.jsx"
+        ));
 
-        const SettingsView = await getModule(m => m.displayName == "SettingsView");
+        const SettingsView = await getModule(
+            m => m.displayName == "SettingsView"
+        );
+        const settingsModule = await getModule(["open", "saveAccountChanges"]);
 
-        inject("bd-like-settings_IPrenderPatch", InstalledProduct.prototype, "render", this.IPrenderPatch);
-        inject("bd-like-settings_IPrenderFooterPatch", InstalledProduct.prototype, "renderFooter", this.IPrenderFooterPatch);
-        inject("bd-like-settings_makeSectionPatch", Settings.prototype, "_makeSection", this.makeSectionPatch);
-        inject("bd-like-settings_getPredicateSectionsPatch", SettingsView.prototype, "getPredicateSections", this.getPredicateSectionsPatch);
+        inject(
+            "bd-like-settings_IPrenderPatch",
+            InstalledProduct.prototype,
+            "render",
+            this.IPrenderPatch
+        );
+        inject(
+            "bd-like-settings_IPrenderFooterPatch",
+            InstalledProduct.prototype,
+            "renderFooter",
+            this.IPrenderFooterPatch
+        );
+        inject(
+            "bd-like-settings_makeSectionPatch",
+            Settings.prototype,
+            "_makeSection",
+            this.makeSectionPatch
+        );
+        inject(
+            "bd-like-settings_getPredicateSectionsPatch",
+            SettingsView.prototype,
+            "getPredicateSections",
+            this.getPredicateSectionsPatch
+        );
+        inject(
+            "bd-like-settings_openSettings",
+            settingsModule,
+            "open",
+            this.openPatch,
+            true
+        );
     }
 
     pluginWillUnload() {
@@ -37,6 +86,7 @@ module.exports = class BDLikeSettings extends Plugin {
         uninject("bd-like-settings_IPrenderFooterPatch");
         uninject("bd-like-settings_makeSectionPatch");
         uninject("bd-like-settings_getPredicateSectionsPatch");
+        uninject("bd-like-settings_openSettings");
         powercord.api.settings.unregisterSettings(this.entityID);
     }
 
@@ -45,46 +95,93 @@ module.exports = class BDLikeSettings extends Plugin {
     }
 
     makeSectionPatch(_, res) {
-        if (res.section.startsWith("pc-") || _this.settings.get("hideSections") === false || _this.settings.get(res.section)) return res;
+        if (
+            res.section.startsWith("pc-") ||
+            _this.settings.get("hideSections") === false ||
+            _this.settings.get(res.section) ||
+            _this.openedSettings === res.section
+        )
+            return (
+                _this.openedSettings === res.section &&
+                    (_this.openedSettings = undefined),
+                res
+            );
         return {};
     }
 
     IPrenderPatch(_, res) {
         if (!this.props.isEnabled || this.props.isAPlugin === false) return res;
-        if (!this.props.hasSettings || !this.state || !this.state.renderSettings) return res;
+        if (
+            !this.props.hasSettings ||
+            !this.state ||
+            !this.state.renderSettings
+        )
+            return res;
         const Settings = this.props.settingsTab.render;
-        return React.createElement(Card, {
-            className: "powercord-product"
-        }, [
-            this.renderHeader(),
-            React.createElement(Divider),
-            React.createElement('div', { style: { display: "flex", "flex-direction": "column" } },
-                React.createElement('div', {
-                    style: {
-                        width: "24px",
-                        float: "right",
-                        "align-self": "flex-end",
-                        margin: "5px"
-                    }
-                },
-                    React.createElement(Tooltip, { text: Messages.CLOSE },
-                        React.createElement(Clickable, {
-                            onClick: () => {
-                                this.setState({ renderSettings: false });
+        return React.createElement(
+            Card,
+            {
+                className: "powercord-product"
+            },
+            [
+                this.renderHeader(),
+                React.createElement(Divider),
+                React.createElement(
+                    "div",
+                    { style: { display: "flex", "flex-direction": "column" } },
+                    React.createElement(
+                        "div",
+                        {
+                            style: {
+                                width: "24px",
+                                float: "right",
+                                "align-self": "flex-end",
+                                margin: "5px"
                             }
-                        }, React.createElement(Close))
+                        },
+                        React.createElement(
+                            Tooltip,
+                            { text: Messages.CLOSE },
+                            React.createElement(
+                                Clickable,
+                                {
+                                    onClick: () => {
+                                        this.setState({
+                                            renderSettings: false
+                                        });
+                                    }
+                                },
+                                React.createElement(Close)
+                            )
+                        )
+                    ),
+                    React.createElement(
+                        "div",
+                        null,
+                        React.createElement(
+                            _this.ErrorBoundary,
+                            null,
+                            React.createElement(Settings)
+                        )
                     )
-                ),
-                React.createElement("div", null, React.createElement(_this.ErrorBoundary, null, React.createElement(Settings)))
-            )
-        ]);
+                )
+            ]
+        );
     }
 
     IPrenderFooterPatch(_, res) {
-        if (res == null || !this.props.isEnabled || this.props.isAPlugin === false || this.props.hasSettings === false) return res;
+        if (
+            res === null ||
+            !this.props.isEnabled ||
+            this.props.isAPlugin === false ||
+            this.props.hasSettings === false
+        )
+            return res;
         if (!this.props.plugin) {
-            const plugin = [...powercord.pluginManager.plugins.values()].find(p =>
-                JSON.stringify(p.manifest) == JSON.stringify(this.props.product)
+            const plugin = [...powercord.pluginManager.plugins.values()].find(
+                p =>
+                    JSON.stringify(p.manifest) ==
+                    JSON.stringify(this.props.product)
             );
             if (!plugin) {
                 this.props.isAPlugin = false;
@@ -92,34 +189,57 @@ module.exports = class BDLikeSettings extends Plugin {
             }
             this.props.plugin = plugin;
         }
-        if (this.props.plugin.entityID.startsWith("pc-") || _this.settings.get(this.props.plugin.entityID)) {
+        if (
+            this.props.plugin.entityID.startsWith("pc-") ||
+            _this.settings.get(this.props.plugin.entityID)
+        ) {
             this.props.hasSettings = false;
             return res;
         }
         const tabs = Object.values(powercord.api.settings.tabs);
         if (
-            (this.props.settingsTab = powercord.api.settings.tabs[this.props.plugin.entityID] ||
-                tabs.find(t => t.category == this.props.plugin.entityID))
-            && this.props.settingsTab)
+            (this.props.settingsTab =
+                powercord.api.settings.tabs[this.props.plugin.entityID] ||
+                tabs.find(t => t.category == this.props.plugin.entityID)) &&
+            this.props.settingsTab
+        )
             this.props.hasSettings = true;
         else this.props.hasSettings = false;
         if (this.props.hasSettings) {
-            const buttons = findInReactTree(res, c => c.props && c.props.className == "buttons");
-            if (!Array.isArray(buttons.props.children)) buttons.props.children = [buttons.props.children];
+            const buttons = findInReactTree(
+                res,
+                c => c.props && c.props.className == "buttons"
+            );
+            if (!Array.isArray(buttons.props.children))
+                buttons.props.children = [buttons.props.children];
             buttons.props.children.unshift(
-                React.createElement(Button, {
-                    onClick: () => {
-                        this.setState({ renderSettings: true });
+                React.createElement(
+                    Button,
+                    {
+                        onClick: () => {
+                            this.setState({ renderSettings: true });
+                        },
+                        color: Button.Colors.BRAND,
+                        look: Button.Looks.FILLED,
+                        size: Button.Sizes.SMALL,
+                        style: { left: "-5px" }
                     },
-                    color: Button.Colors.BRAND,
-                    look: Button.Looks.FILLED,
-                    size: Button.Sizes.SMALL,
-                    style: { "left": "-5px" }
-                }, Messages.SETTINGS)
+                    Messages.SETTINGS
+                )
             );
             buttons.props.style = { display: "flex", "flex-direction": "row" };
             if (!this.state) this.setState({ renderSettings: false });
         }
         return res;
     }
-}
+
+    openPatch(args) {
+        if (
+            !args[0].startsWith("pc-") &&
+            powercord.api.settings.tabs[args[0]] &&
+            !_this.settings.get(args[0])
+        )
+            _this.openedSettings = args[0];
+        return args;
+    }
+};
